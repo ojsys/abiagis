0eedb49148491bd9d68ef6fbbaf247aca902eddf
Onah Jonah <onahjonah@gmail.com>
2023-12-20 19:16:19 +0100

    made some changes

--- abiagis/settings.py
+++ abiagis/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/'),)
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
